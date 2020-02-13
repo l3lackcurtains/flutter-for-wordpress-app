02fff4c82e9b0e41fcc27b6403b8ad0202263daa
l3lackcurtains <9688803+l3lackcurtains@users.noreply.github.com>
2020-02-13 14:11:55 -0700

    Updated app and readme

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,18 +1,28 @@
 library constants;
 
+// Your wordpress website URL
 const String WORDPRESS_URL = "https://flutterblog.crumet.com";
+
+// Featured category ID (for Home Screen top section)
 const int FEATURED_ID = 2;
+
+// Tab 2 page category name
 const String PAGE2_CATEGORY_NAME = "Lifestyle";
+
+// Tab 2 page category ID
 const int PAGE2_CATEGORY_ID = 6;
 
+// Custom categories in search tab
+// Array in format
+// ["Category Name", "Image Link", "Category ID"]
 const List<dynamic> CUSTOM_CATEGORIES = [
-  ["Lifestyle", "assets/boxed/lifestyle.png", 1],
-  ["Fashion", "assets/boxed/fashion.png", 1],
-  ["Music", "assets/boxed/music.png", 1],
-  ["Photography", "assets/boxed/photography.png", 1],
-  ["Sport", "assets/boxed/sport.png", 1],
-  ["World", "assets/boxed/world.png", 1],
-  ["Health", "assets/boxed/health.png", 1],
-  ["Travel", "assets/boxed/travel.png", 1],
-  ["Recipies", "assets/boxed/recipies.png", 1],
+  ["Lifestyle", "assets/boxed/lifestyle.png", 6],
+  ["Fashion", "assets/boxed/fashion.png", 12],
+  ["Music", "assets/boxed/music.png", 14],
+  ["Photography", "assets/boxed/photography.png", 15],
+  ["Sport", "assets/boxed/sport.png", 13],
+  ["World", "assets/boxed/world.png", 11],
+  ["Health", "assets/boxed/health.png", 8],
+  ["Travel", "assets/boxed/travel.png", 7],
+  ["Recipies", "assets/boxed/recipies.png", 10],
 ];
